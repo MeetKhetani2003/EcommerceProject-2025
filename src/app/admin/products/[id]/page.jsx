ab5185e16d50abd5b0d26748b6f181c227756558
MeetKhetani2003 <meetkhetani1111@gmail.com>
2025-12-20 20:19:53 +0530

    Profile Page hasbeen done

--- src/app/admin/products/[id]/page.jsx
+++ src/app/admin/products/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import ProductForm from "../components/ProductForm";
+import ProductForm from "../../components/ProductForm";
 import { useApiClient } from "../../lib/api";
 
 export default function ProductDetailPage({ params }) {
