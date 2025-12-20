4c024ef06037f7834328f3e6299d30bc56d6476d
MeetKhetani2003 <meetkhetani1111@gmail.com>
2025-12-20 20:17:54 +0530

    Profile Page hasbeen done

--- src/app/admin/users/[id]/page.jsx
+++ src/app/admin/users/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { useApiClient } from "../lib/api";
+import { useApiClient } from "../../lib/api";
 
 export default function UserDetail({ params }) {
   const id = params.id;
